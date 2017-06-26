71b6f4ed5f277b56c0121142b5fce1eac09eb679
ployer900 <yuhongliang900@163.com>
2017-06-26 21:32:33 +0800

    修改infiniteLoad,store库

--- InfiniteLoad.js
+++ InfiniteLoad.js
@@ -4,93 +4,85 @@
 * @Last Modified by:   yuhongliang
 * @Last Modified time: 2017-06-26 17:24:35
 */
-
 'use strict';
 
-/**
- *
- * 滚动分页加载
- *
- * 
- */
+//构造函数
+var InfiniteLoad = function() {
+    this.init.apply(this, arguments);
+}
 
-;(function(global, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(factory);
-    } else if (typeof exports === 'object' && module.exports) {
-        module.exports = factory();
-    } else {
-        global.ScrollLoad = factory();
-    }
-}(window, function() {
-    var ScrollLoad = function() {
-        this.init.apply(this, arguments);
-    }
-    //原型函数
-    ScrollLoad.prototype = {
-        constructor: ScrollLoad,
-        init: function(el, fetchData) {
-            var self = this;
-            this.el = el;
-            this.fetchData = fetchData;
-            this.isScroll = false;
-            $(window).on('scroll', function() {
-                self.throttle(function() {
-                    if (self.isLoad(self.el)) {
-                        self.fetchData();
-                        self.isScroll = true;
-                    }
+//初始化
+InfiniteLoad.prototype.init = function(el, fetch) {
+    var self = this;
+    self.el = el;
+    self.fetch = fetch;
+    self.isScroll = false;
+    $(window).on('scroll', function() {
+        self.throttle(function() {
+            if (self.startLoading(self.el)) {
+                self.fetch();
+                self.isScroll = true;
+            }
+        }, 500, 500)();
+    });
+    self.fetch();
+}
 
-                }, 500, 500)();
-            });
-            this.fetchData();
-        },
-    }
-    //节流函数
-    ScrollLoad.prototype.throttle = function(fn, delay, timeout) {
-        var timer = null,
-            previous = null;
-        return function() {
-            var now = +new Date();
-            if (!previous) previous = now;
-            if (timeout && (now - previous) > delay) {
+//节流函数
+InfiniteLoad.prototype.throttle = function(fn, delay, timeout) {
+    var timer = null,
+        previous = null;
+    return function() {
+        var now = +new Date();
+        if (!previous) previous = now;
+        if (timeout && (now - previous) > delay) {
+            fn();
+            clearTimeout(timer);
+        } else {
+            clearTimeout(timer);
+            timer = setTimeout(function() {
                 fn();
-                clearTimeout(timer);
-            } else {
-                clearTimeout(timer);
-                timer = setTimeout(function() {
-                    fn();
-                }, timeout);
-            }   
+            }, timeout);
         }
     }
-    //元素是否可见
-    ScrollLoad.prototype.isVisible = function(node, threshold) {
-        var top = this.getElemPos(node).top,
-            height = node.offsetHeight,
-            innerHeight = window.innerHeight,
-            scrollDis = window.pageYOffset,
-            threshold = threshold || 0;
-        return (scrollDis + threshold > (top - innerHeight)) && (scrollDis < (top + height));
+}
+
+//元素是否可见
+InfiniteLoad.prototype.isVisible = function(node, threshold) {
+    var top = this.getLoadingIconPosition(node).top,
+        height = node.offsetHeight,
+        innerHeight = window.innerHeight,
+        scrollYOffset = window.pageYOffset,
+        threshold = threshold || 0;
+    return (scrollYOffset + threshold > (top - innerHeight)) && (scrollYOffset < (top + height));
+}
+
+//元素位置
+InfiniteLoad.prototype.getLoadingIconPosition = function(node) {
+    var left = 0,
+        top = 0;
+    while(node && node.offsetParent) {
+        left += node.offsetLeft;
+        top += node.offsetTop;
+        node = node.offsetParent;
     }
-    //元素位置
-    ScrollLoad.prototype.getElemPos = function(node) {
-        var left = 0,
-            top = 0;
-        while(node && node.offsetParent) {
-            left += node.offsetLeft;
-            top += node.offsetTop;
-            node = node.offsetParent;
-        }
-        return {left: left, top: top};
-    }
-    //是否加载过
-    ScrollLoad.prototype.isLoad = function(node) {
-        return this.isVisible(node, 1000) && !this.isScroll;
-    }
-    //关闭scroll
-    ScrollLoad.prototype.offScroll = function() {
-        $(window).off('scroll');
-    }
-    return ScrollLoad;
-}));
+    return {left: left, top: top};
+}
+
+//是否加载
+InfiniteLoad.prototype.startLoading = function(node) {
+    return this.isVisible(node, 1000) && !this.isScroll;
+}
+
+//关闭scroll
+InfiniteLoad.prototype.offScroll = function() {
+    $(window).off('scroll');
+}
+
+//加载完毕
+InfiniteLoad.prototype.loadEnded = function(options) {
+    $(this.el).css(options);
+    this.offScroll();
+}
+
+module.exports = InfiniteLoad;
--- store.js
+++ store.js
@@ -12,44 +12,70 @@ var store = module.exports = {
     //是否支持history.replaceState和history.state
     isSupportHistoryState: window.history.replaceState && window.history.state,
     isSupportLocalStorage: window.localStorage,
+    isSupportSessionStorage: window.sessionStorage,
 
     //是否为字符串类型
     isString: function(v) {
         return typeof v === 'string';
     },
-
-    //添加item
-    add: function(key, value) {
-        if (this.isSupportLocalStorage){
+    //存储session周期的数据
+    addSession: function(key, value) {
+        if (this.isSupportSessionStorage){
             if (this.isString(key) && this.isString(value)) {
-                window.localStorage.setItem(key,value);
+                window.sessionStorage.setItem(key,value);
             } else throw new Error('key,value必须为字符串数据类型');
         } else if (this.isSupportHistoryState) {
             var state = window.history.state;
             if (!state) state = {};
             state[key] = value;
             window.history.replaceState(state, '', '');
-        } else throw new Error('不支持localStorage,history.state');
+        } else throw new Error('不支持sessionStorage,history.state');
     },
-
-    //获取item
-    get: function(key) {
-        if (this.isSupportLocalStorage) return window.localStorage.getItem(key);
+    //获取session周期的数据
+    getSession: function(key) {
+        if (this.isSupportSessionStorage) return window.sessionStorage.getItem(key);
         else if (this.isSupportHistoryState) return window.history.state[key];
-        else throw new Error('不支持localStorage,history.state');
+        else throw new Error('不支持sessionStorage,history.state');
     },
-
+    //永久性存储数据
+    addPermanent: function() {
+        if (!this.isSupportLocalStorage) {
+            throw new Error('不支持localStorage');
+            return;
+        }
+        if (this.isString(key) && this.isString(value)) {
+            window.localStorage.setItem(key,value);
+        } else throw new Error('key,value必须为字符串数据类型');
+    },
+    //获取item
+    getPermanent: function(key) {
+        if (!this.isSupportLocalStorage) {
+            throw new Error('不支持localStorage');
+            return;
+        }
+        return window.localStorage.getItem(key);
+    },
+    //删除session
+    deleteSession: function(key) {
+        if (!this.sessionStorage) {
+            throw new Error('不支持sessionStorage');
+            return;
+        }
+        return window.sessionStorage.removeItem(key);
+    },
     //删除item
-    delete: function(key) {
-        if (this.isSupportLocalStorage) window.localStorage.removeItem(key);
-        else if (this.isSupportHistoryState) delete window.history.state[key];
-        else throw new Error('不支持localStorage,history.state');
+    deletePermanent: function(key) {
+        if (!this.isSupportLocalStorage) {
+            throw new Error('不支持localStorage');
+            return;
+        }
+        return window.localStorage.removeItem(key);
     },
-
     //清除
     clear: function() {
         if (this.isSupportLocalStorage) window.localStorage.clear();
+        else if (this.isSupportSessionStorage) window.sessionStorage.clear();
         else if (this.isSupportHistoryState) window.history.replaceState(null, '', '');
-        else throw new Error('不支持localStorage,history.state');
+        else throw new Error('不支持localStorage,sessionStorage,history.state');
     }
 };
